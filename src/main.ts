1d131a9e441fc60a81ad730eb53067d3cb71ca29
Myeongcheol Yu <mspringmspring@naver.com>
2022-01-19 18:57:40 +0900

    initail-setting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { Module } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import {} from '@deegda/protocol/type/user';
+
+@Module({
+    imports: []
+})
+class AppModule {}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const expressApp = express();
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+
+    const options = new DocumentBuilder().setTitle('API Docs').setDescription('Davoda API Docs').setVersion('1.0').build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api/v1', app, document);
+
+    await app.listen(3000);
 }
 bootstrap();
